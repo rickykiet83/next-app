d82a2a193283726ee5f9e081ce174437a1ed9f0e
Kiet Pham <kiet.pham@lapisit.com.au>
2023-09-12 21:24:51 +1000

    DaisyUI

---
+++ app/components/AddToCard.tsx
@@ -0,0 +1,13 @@
+'use client';
+
+import React from 'react';
+
+const AddToCard = () => {
+	return (
+		<div>
+			<button className='btn btn-primary'>Add to Card</button>
+		</div>
+	);
+};
+
+export default AddToCard;
---
+++ app/components/ProductCard/ProductCard.tsx
@@ -0,0 +1,12 @@
+import AddToCard from './../AddToCard';
+import React from 'react';
+
+const ProductCard = () => {
+	return (
+		<div>
+			<AddToCard />
+		</div>
+	);
+};
+
+export default ProductCard;
