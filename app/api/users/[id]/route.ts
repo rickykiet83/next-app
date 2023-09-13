290ec9ed0b0c31cded08c9c95a26ff19713c8335
Kiet Pham <kiet.pham@lapisit.com.au>
2023-09-13 11:58:30 +1000

    Update an Object

--- app/api/users/[id]/route.ts
+++ app/api/users/[id]/route.ts
@@ -9,3 +9,14 @@ export function GET(
 
 	return NextResponse.json({ id: 1, name: 'Kiet' });
 }
+
+
+export async function PUT(request: NextRequest,
+	{ params }: { params: { id: number } }
+	) {
+	const body = await request.json();
+	if (!body.name) return NextResponse.json({error: 'Name is required'}, {status: 400});
+	if (params.id > 10) return NextResponse.json({error: 'User not found'}, {status: 400});
+
+	return NextResponse.json({id: 1, name: body.name}, {status: 200});
+}
