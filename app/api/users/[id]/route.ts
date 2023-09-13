8c08529b2442b01889bd7f51a840a4ce1a536e9c
Kiet Pham <kiet.pham@lapisit.com.au>
2023-09-13 12:12:03 +1000

    Validating requests with Zod

--- app/api/users/[id]/route.ts
+++ app/api/users/[id]/route.ts
@@ -1,5 +1,7 @@
 import { NextRequest, NextResponse } from 'next/server';
 
+import schema from './../schema';
+
 export function GET(
 	request: NextRequest,
 	{ params }: { params: { id: number } }
@@ -15,7 +17,10 @@ export async function PUT(request: NextRequest,
 	{ params }: { params: { id: number } }
 	) {
 	const body = await request.json();
-	if (!body.name) return NextResponse.json({error: 'Name is required'}, {status: 400});
+	const validation = schema.safeParse(body);
+	if (!validation.success)
+		return NextResponse.json(validation.error.errors, {status: 400});
+
 	if (params.id > 10) return NextResponse.json({error: 'User not found'}, {status: 400});
 
 	return NextResponse.json({id: 1, name: body.name}, {status: 200});
--- app/api/users/route.ts
+++ app/api/users/route.ts
@@ -1,6 +1,8 @@
 import { NextApiRequest, NextApiResponse } from 'next';
 import { NextRequest, NextResponse } from 'next/server';
 
+import schema from './schema';
+
 export function GET(request: NextRequest, response: NextResponse ) {
 	return NextResponse.json(
 		[
@@ -11,7 +13,9 @@ export function GET(request: NextRequest, response: NextResponse ) {
 
 export async function POST(request: NextRequest) {
 	const body = await request.json();
-	if (!body.name) return NextResponse.json({error: 'Name is required'}, {status: 400});
+	const validation = schema.safeParse(body);
+	if (!validation.success)
+		return NextResponse.json(validation.error.errors, {status: 400});
 
 	return NextResponse.json({id: 1, name: body.name}, {status: 201});
 }
