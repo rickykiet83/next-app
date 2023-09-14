37ef3837af4f2279f1a58e3ded76bd1d4e94e6f6
Kiet Pham <kiet.pham@lapisit.com.au>
2023-09-14 13:04:41 +1000

    Updating Data

--- app/api/users/[id]/route.ts
+++ app/api/users/[id]/route.ts
@@ -19,16 +19,31 @@ export async function GET(
 
 
 export async function PUT(request: NextRequest,
-	{ params }: { params: { id: number } }
+	{ params }: { params: { id: string } }
 ) {
 	const body = await request.json();
 	const validation = schema.safeParse(body);
 	if (!validation.success)
 		return NextResponse.json(validation.error.errors, { status: 400 });
 
-	if (params.id > 10) return NextResponse.json({ error: 'User not found' }, { status: 400 });
+	const user = prisma.user.findUnique({
+		where: { id: parseInt(params.id) }
+	}
+	);
 
-	return NextResponse.json({ id: 1, name: body.name }, { status: 200 });
+
+	if (!user) return NextResponse.json({ error: 'User not found' }, { status: 400 });
+
+	const updatedUser = await prisma.user.update({
+		where: { id: parseInt(params.id) },
+		data: {
+			name: body.name,
+			email: body.email
+		}
+	});
+
+
+	return NextResponse.json(updatedUser, { status: 200 });
 }
 
 
