4b33368c04e76dcdfb51f05bee01df6183069369
Kiet Pham <kiet.pham@lapisit.com.au>
2023-09-14 12:47:19 +1000

    Getting Data

--- app/api/users/[id]/route.ts
+++ app/api/users/[id]/route.ts
@@ -1,29 +1,34 @@
 import { NextRequest, NextResponse } from 'next/server';
 
-import schema from './../schema';
+import { prisma } from "@/prisma/client";
+import schema from "./../schema";
 
-export function GET(
+export async function GET(
 	request: NextRequest,
-	{ params }: { params: { id: number } }
+	{ params }: { params: { id: string } }
 ) {
-	if (params.id > 10)
+	const user = await prisma.user.findUnique({
+		where: { id: parseInt(params.id) }
+	});
+
+	if (!user)
 		return NextResponse.json({ error: 'User not found' }, { status: 404 });
 
-	return NextResponse.json({ id: 1, name: 'Kiet' });
+	return NextResponse.json(user);
 }
 
 
 export async function PUT(request: NextRequest,
 	{ params }: { params: { id: number } }
-	) {
+) {
 	const body = await request.json();
 	const validation = schema.safeParse(body);
 	if (!validation.success)
-		return NextResponse.json(validation.error.errors, {status: 400});
+		return NextResponse.json(validation.error.errors, { status: 400 });
 
-	if (params.id > 10) return NextResponse.json({error: 'User not found'}, {status: 400});
+	if (params.id > 10) return NextResponse.json({ error: 'User not found' }, { status: 400 });
 
-	return NextResponse.json({id: 1, name: body.name}, {status: 200});
+	return NextResponse.json({ id: 1, name: body.name }, { status: 200 });
 }
 
 
--- app/api/users/route.ts
+++ app/api/users/route.ts
@@ -1,21 +1,20 @@
-import { NextApiRequest, NextApiResponse } from 'next';
-import { NextRequest, NextResponse } from 'next/server';
+import { NextRequest, NextResponse } from "next/server";
 
-import schema from './schema';
+import { prisma } from "@/prisma/client";
+import schema from "./schema";
 
-export function GET(request: NextRequest, response: NextResponse ) {
-	return NextResponse.json(
-		[
-		{ id: 1, name: 'Kiet' },
-		{ id: 2, name: 'Dan' },
-	]);
+export async function GET(request: NextRequest, response: NextResponse) {
+	const users = await prisma.user.findMany();
+
+
+	return NextResponse.json(users);
 }
 
 export async function POST(request: NextRequest) {
 	const body = await request.json();
 	const validation = schema.safeParse(body);
 	if (!validation.success)
-		return NextResponse.json(validation.error.errors, {status: 400});
+		return NextResponse.json(validation.error.errors, { status: 400 });
 
-	return NextResponse.json({id: 1, name: body.name}, {status: 201});
+	return NextResponse.json({ id: 1, name: body.name }, { status: 201 });
 }
