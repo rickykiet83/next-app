da8b094f568adf854584871d98827d81e54bb3e4
Kiet Pham <kiet.pham@lapisit.com.au>
2023-09-17 21:32:02 +1000

    Registering Users

---
+++ app/api/register/route.ts
@@ -0,0 +1,37 @@
+import { NextRequest, NextResponse } from 'next/server';
+
+import bcrypt from 'bcrypt';
+import { prisma } from '@/prisma/client';
+import { z } from 'zod';
+
+const schema = z.object({
+  email: z.string().email(),
+  password: z.string().min(5),
+})
+
+export async function POST(request: NextRequest) {
+  const body = await request.json();
+  const validation = schema.safeParse(body);
+  if (!validation.success)
+    return NextResponse.json(validation.error.errors, { status: 404 });
+
+  const user = await prisma.user.findUnique({ where: { email: body.email } });
+  if (user)
+    return NextResponse.json({
+      error: 'User already exists',
+      status: 400
+    });
+
+  const hashedPassword = await bcrypt.hash(body.password, 10);
+
+  const newUser = await prisma.user.create({
+    data: {
+      name: body.name,
+      email: body.email,
+      hashedPassword
+    }
+  });
+
+  return NextResponse.json({ email: newUser.email });
+
+}
