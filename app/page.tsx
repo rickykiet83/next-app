0f00143aec6ea1f81fcacd37804a75ee505d08c9
Kiet Pham <kiet.pham@lapisit.com.au>
2023-09-17 15:09:49 +1000

    Accessing sessions on the server

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,15 @@
 import Image from 'next/image';
 import Link from 'next/link';
 import ProductCard from './components/ProductCard/ProductCard';
+import { authOptions } from './api/auth/[...nextauth]/route';
+import { getServerSession } from 'next-auth';
 
-export default function Home() {
+export default async function Home() {
+	const session = await getServerSession(authOptions);
+
 	return (
 		<main>
+			<h1>Hello {session && <span>{session.user?.name}</span>}</h1>
 			<ProductCard />
 		</main>
 	);
