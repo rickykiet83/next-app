8e7fbd20152f22de5d726159bc1962158312554f
Kiet Pham <kiet.pham@lapisit.com.au>
2023-09-18 21:54:08 +1000

    Optimizing Images

--- app/page.tsx
+++ app/page.tsx
@@ -2,15 +2,24 @@ import Image from 'next/image';
 import Link from 'next/link';
 import ProductCard from './components/ProductCard/ProductCard';
 import { authOptions } from './api/auth/[...nextauth]/route';
+import coffee from '@/public/images/coffee.jpg';
 import { getServerSession } from 'next-auth';
 
 export default async function Home() {
 	const session = await getServerSession(authOptions);
 
 	return (
-		<main>
+		<main className='relative h-screen'>
 			<h1>Hello {session && <span>{session.user?.name}</span>}</h1>
-			<ProductCard />
+			<Image
+				src='https://bit.ly/react-cover'
+				alt='Coffee'
+				fill
+				className='object-cover'
+				sizes='(max-width: 480px) 100vw, (max-width: 786px) 50vw, 33vw'
+				quality={100}
+				priority={true}
+			/>
 		</main>
 	);
 }
